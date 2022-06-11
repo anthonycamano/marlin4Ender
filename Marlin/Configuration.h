41c138a074adeb76856c27fc5f5868c0ad354072
Anthony Camano-Enriquez <43197555+anthonycamano@users.noreply.github.com>
2022-06-11 13:34:20 -0500

    fixing a few oopsies and sound

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2326,8 +2326,8 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 100
-#define LCD_FEEDBACK_FREQUENCY_HZ 500
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
+#define LCD_FEEDBACK_FREQUENCY_HZ 5000
 
 //=============================================================================
 //======================== LCD / Controller Selection =========================
